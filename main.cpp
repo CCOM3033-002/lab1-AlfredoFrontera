c4a2c8008914adfc7028a18d20176e9bf941c1f0
AlfredoFrontera <alfredo.frontera2@upr.edu>
2025-02-13 01:36:54 +0000

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+/*
+Asignación 1: Binary Digit
+Nombre:Alfredo F. Frontera Del Valle
+Núm. Est: 801-21-1568 */
 
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "This program calculates the Gross and Net Box Office Profit and the Distributor's Profit of a movie playing in the theater."<< endl << endl;
+}
