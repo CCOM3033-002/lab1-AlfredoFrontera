9f3eb5312de64f53f9f4b589bef67eb56f3b8edf
AlfredoFrontera <alfredo.frontera2@upr.edu>
2025-02-13 01:38:42 +0000

    Commit 2

--- main.cpp
+++ main.cpp
@@ -11,3 +11,17 @@ using namespace std;
 int main(){
     cout << "This program calculates the Gross and Net Box Office Profit and the Distributor's Profit of a movie playing in the theater."<< endl << endl;
 }
+
+string movieName;
+int adultTickets, childTickets;
+const double ADULT_TICKET_PRICE = 10.00;
+const double CHILD_TICKET_PRICE = 6.00;
+const double THEATER_PERCENTAGE = 0.20;
+
+cout << "Enter the name of the movie: ";
+getline(cin, movieName);
+cout << "Enter the amount of adult tickets sold: ";
+cin >> adultTickets;
+cout << "Enter the amount of child tickets sold: ";
+cin >> childTickets;
+
